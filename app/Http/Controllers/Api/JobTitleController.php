7193bc4751700a6800038e598625dd0c81fe9403
unknown <Mohammad Khanfar>
2021-07-26 09:40:22 -0700

    Add Some notes

---
+++ app/Http/Controllers/Api/JobTitleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\JobTitle;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class JobTitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobTitle $jobTitle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JobTitle  $jobTitle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobTitle $jobTitle)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/TypeExamController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\TypeExam;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TypeExamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TypeExam  $typeExam
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeExam $typeExam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TypeExam  $typeExam
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypeExam $typeExam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TypeExam  $typeExam
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeExam $typeExam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TypeExam  $typeExam
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeExam $typeExam)
+    {
+        //
+    }
+}
